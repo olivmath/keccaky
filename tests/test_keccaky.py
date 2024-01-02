6ab111a77a32b605d01bb9ac16890bd6353998e4
Lucas Oliveira <olivmath@protonmail.com>
2024-01-02 20:21:45 -0300

    update: imports

--- tests/test_keccaky.py
+++ tests/test_keccaky.py
@@ -1,13 +1,10 @@
-import keccaky
-from keccaky.wip import KeccakHash
+from keccaky import KeccakHash
 
 
-def test_mock_result():
+
+def test_hello_world():
     msg = "Hello, world"
     result = "db99debe7fc546756227481ecaf5136f5b86180d99c5666a14421367c7187e5c"
-
-    assert keccaky.keccak256(msg) == bytes.fromhex(result)
-    assert keccaky.keccak256(msg).hex() == result
 
     k = KeccakHash()
     x = k.digest(msg)
