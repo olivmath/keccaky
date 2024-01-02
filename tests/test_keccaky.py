9441eb71da5347ca2514b6007b31388c88c5e7d4
Lucas Oliveira <olivmath@protonmail.com>
2024-01-02 16:31:11 -0300

    update: tests to support new keccak class

--- tests/test_keccaky.py
+++ tests/test_keccaky.py
@@ -1,11 +1,14 @@
 import keccaky
+from keccaky.wip import KeccakHash
 
 
 def test_mock_result():
-    assert keccaky.keccak256("Hello, world") == bytes.fromhex(
-        "db99debe7fc546756227481ecaf5136f5b86180d99c5666a14421367c7187e5c"
-    )
-    assert (
-        keccaky.keccak256("Hello, world").hex()
-        == "db99debe7fc546756227481ecaf5136f5b86180d99c5666a14421367c7187e5c"
-    )
+    msg = "Hello, world"
+    result = "db99debe7fc546756227481ecaf5136f5b86180d99c5666a14421367c7187e5c"
+
+    assert keccaky.keccak256(msg) == bytes.fromhex(result)
+    assert keccaky.keccak256(msg).hex() == result
+
+    k = KeccakHash()
+    x = k.digest(msg)
+    assert bytes(x).hex() == result
