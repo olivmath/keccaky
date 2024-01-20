60eb4f8d3e8284d144a7b4a10eb69e0444d7642e
Lucas Oliveira <olivmath@protonmail.com>
2024-01-20 02:02:45 -0300

    update: lint

--- tests/benchmark/test_keccaky_benchmark.py
+++ tests/benchmark/test_keccaky_benchmark.py
@@ -3,7 +3,6 @@ import pytest
 import time
 
 
-
 @pytest.mark.benchmark(group="HashCreation", timer=time.time)
 def test_create_10_hash(benchmark):
     benchmark(lambda: [hash_it_bytes(str(i).encode()).hex() for i in range(10)])
